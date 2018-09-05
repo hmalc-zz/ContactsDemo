a1d27e9f48211a03c63d0717fa6b791d71cedfc6
hmalc <hayden.malcomson@me.com>
2018-09-05 00:24:49 -0600

    UITest created and passing

--- ShowbieCodingChallengeUITests/ShowbieCodingChallengeUITests.swift
+++ ShowbieCodingChallengeUITests/ShowbieCodingChallengeUITests.swift
@@ -23,11 +23,39 @@ class ShowbieCodingChallengeUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testEmailEntryUserFlow() {
         
+        let exampleEmail = "test@me.ca"
         
+        let app = XCUIApplication()
+        let firstCell = app.tables.cells.firstMatch
+        firstCell.tap()
+        let textField = app.textFields.firstMatch
+        textField.tap()
+        textField.clearText()
+        textField.typeText(exampleEmail)
+        
+        app.navigationBars["Detail"].buttons["Master"].tap()
+        
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts[exampleEmail].tap()
         
         
     }
     
+}
+
+extension XCUIElement {
+    func clearText(andReplaceWith newText:String? = nil) {
+        tap()
+        tap() //When there is some text, its parts can be selected on the first tap, the second tap clears the selection
+        press(forDuration: 1.0)
+        let selectAll = XCUIApplication().menuItems["Select All"]
+        //For empty fields there will be no "Select All", so we need to check
+        if selectAll.waitForExistence(timeout: 0.5), selectAll.exists {
+            selectAll.tap()
+            typeText(String(XCUIKeyboardKey.delete.rawValue))
+        }
+        if let newVal = newText { typeText(newVal) }
+    }
 }
